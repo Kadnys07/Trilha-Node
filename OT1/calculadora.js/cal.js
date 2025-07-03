bac59de6326afb27fddfff34accea5b7f553d461
kadnys_martins <kadnysm@gmail.com>
2025-07-03 20:53:33 -0300

    First commit

---
+++ OT1/calculadora.js/cal.js
@@ -0,0 +1,9 @@
+let somaFunc = require("./somar.js");
+let subtracaoFunc = require("./subtrair.js");
+let multiplicacaoFunc = require("./multiplicar.js");
+let divisaoFunc = require("./dividir.js");
+
+console.log("O valor da soma é: "+somaFunc(5, 3));
+console.log("O valor da subtração é: "+subtracaoFunc(5, 3));
+console.log("O valor da multiplicação é: "+multiplicacaoFunc(5, 3));
+console.log("O valor da divisão é: "+divisaoFunc(5, 3));
---
+++ OT1/calculadora.js/dividir.js
@@ -0,0 +1,4 @@
+const div = function(a, b) {
+    return (a / b).toFixed(2);
+}
+module.exports = div;
